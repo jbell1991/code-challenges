c48445640a562514d28d0838115cffa8f6c5942a
jbell1991 <joseph.louis.bell@gmail.com>
2020-08-08 15:10:32 -0400

    counting duplicates solution and tests

---
+++ counting_duplicates/counting_duplicates.py
@@ -0,0 +1,22 @@
+def duplicate_count(text):
+    # empty dictionary
+    dict = {}
+    # make text lowercase
+    text = text.lower()
+    # iterate over each character in the text
+    for char in text:
+        # if the character is not in the dictionary
+        if char not in dict:
+            # create a key and assign the value to 1
+            dict[char] = 1
+        else:
+            # add 1 to the value
+            dict[char] += 1
+    # count how many characters have values > 1
+    count = 0
+    # iterate over each value in the dictionary values
+    for value in dict.values():
+        # if the value is greater than 1 it counts a duplicate
+        if value > 1:
+            count += 1
+    return count
